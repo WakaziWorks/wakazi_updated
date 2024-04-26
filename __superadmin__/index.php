3a8b8f9d0f651275f4dc3fa2f7a154e6b32070f2
skeeperloyaltie <gugod254@gmail.com>
2024-04-26 10:19:54 +0300

    change homepage - to stop goig to coming soon page

--- __superadmin__/index.php
+++ __superadmin__/index.php
@@ -1,68 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include("../__screens/__headers/header.php");
+?>
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Wakazi Works</title>
-    <link rel="icon" type="image/x-icon" href="static/assets/favicon.ico" />
-
-    <!-- Bootstrap icons-->
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-
-    <!-- Google fonts-->
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <!--<link href="css/styles.css" rel="stylesheet" />-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-
-    <!-- CSS files loading here -->
-    <!-- <link rel="stylesheet" href="../update_wakazi/ -->
-    <link rel="stylesheet" href="../../static/css/styles.css">
-    <style>
-        .navbar-nav {
-            margin-bottom: 0;
-        }
-        #navbar-nav a{
-            color: #ff00ff;
-        }
-        #navbar-nav a:hover{
-            color: #fff;
-            background: #ff00ff;
-            border-radius: 15px;
-        }
-
-        .navbar-light .navbar-nav .nav-link {
-            padding: 8px 10px;
-            /* Reduced padding to bring items closer */
-        }
-
-        .navbar {
-            padding-top: 0;
-            padding-bottom: 0;
-        }
-
-        .container-fluid {
-            padding-left: 0;
-            padding-right: 0;
-        }
-
-        /* Reducing space specifically between the search section and the navigation row */
-        .navbar-expand-lg+.navbar {
-            margin-top: -10px;
-            /* Pulls the second navbar up closer to the first */
-        }
-    </style>
-</head>
 <body id="page-top">
     <header class="header fixed-top">
         <nav class="nav navbar navbar-expand-lg navbar-light">
@@ -123,16 +62,16 @@
                 <!-- Icons for cart, account, and help -->
                 <div class="d-flex accounts-nav">
                     <?php if ($isLoggedIn) : ?>
-                        <!-- <a href="#" class="nav-link">
+                        <a href="#" class="nav-link">
                             <i class="bi bi-cart4" style="padding: 2px;"></i> Cart
-                        </a> -->
+                        </a>
                         <!-- Display user info and logout link -->
-                        <!-- <a href="../../__superadmin__/index.html" class="nav-link" id="user-info">
+                        <a href="" class="nav-link" id="user-info">
                             <i class="bi bi-person-check"></i> <?= $_SESSION['email']; ?>
-                        </a> -->
-                        <!-- <a href="../../__logout/__logout.php" class="nav-link">
+                        </a>
+                        <a href="../../__logout/__logout.php" class="nav-link">
                             <i class="bi bi-box-arrow-right"></i> Logout
-                        </a> -->
+                        </a>
                     <?php else : ?>
                         <!-- Links to show when the user is not logged in -->
                         <a href="../../__auth/__accounts/login.php" class="nav-link" id="popup-trigger">
@@ -157,20 +96,20 @@
                 <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
                     <ul class="navbar-nav" id="navbar-nav">
                         <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="../../index.php">Home</a></li>
-                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="../../__superadmin__/index.html">Products</a></li>
-                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="../../__superadmin__/index.html">Collections</a></li>
-                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="../../__superadmin__/index.html">Features</a></li>
-                        <li class="nav-item d-none d-lg-block" ><a class="nav-link" href="../../__superadmin__/index.html">Blog</a></li>
+                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="">Products</a></li>
+                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="">Collections</a></li>
+                        <li class="nav-item d-none d-lg-block" style="margin-right: 60px;"><a class="nav-link" href="">Features</a></li>
+                        <li class="nav-item d-none d-lg-block" ><a class="nav-link" href="">Blog</a></li>
                         <li class="nav-item dropdown d-lg-none">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Menu
                             </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                 <li><a class="dropdown-item" href="index.php">Home</a></li>
-                                <li><a class="dropdown-item" href="../../__superadmin__/index.html">Products</a></li>
-                                <li><a class="dropdown-item" href="../../__superadmin__/index.html">Collections</a></li>
-                                <li><a class="dropdown-item" href="../../__superadmin__/index.html">Features</a></li>
-                                <li><a class="dropdown-item" href="../../__superadmin__/index.html">Blog</a></li>
+                                <li><a class="dropdown-item" href="">Products</a></li>
+                                <li><a class="dropdown-item" href="">Collections</a></li>
+                                <li><a class="dropdown-item" href="">Features</a></li>
+                                <li><a class="dropdown-item" href="">Blog</a></li>
                             </ul>
                         </li>
                     </ul>
