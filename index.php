0fa1cda99bc50fd36b516515e843931a7a724a67
skeeperloyaltie <gugod254@gmail.com>
2024-04-26 09:56:56 +0300

    footer addressing

--- index.php
+++ index.php
@@ -300,164 +300,142 @@ include("__screens/__headers/header.php")
     </div>
 </section>
 
+<section class="footer-bottom">
 
+    <!-- Remove the container if you want to extend the Footer to full width. -->
+    <div class="my-5">
 
-<!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="container my-5">
+        <!-- Footer -->
+        <footer class="text-center text-lg-start text-white" style="background-color: #431B63">
+            <!-- Section: Social media -->
+            <section class="d-flex justify-content-between p-4" style="background-color: #433595">
+                <!-- Left -->
+                <div class="me-5">
+                    <span>Get connected with us on social networks:</span>
+                </div>
+                <!-- Left -->
 
-  <!-- Footer -->
-  <footer
-          class="text-center text-lg-start text-white"
-          style="background-color: #431B63"
-          >
-    <!-- Section: Social media -->
-    <section
-             class="d-flex justify-content-between p-4"
-             style="background-color: #433595"
-             >
-      <!-- Left -->
-      <div class="me-5">
-        <span>Get connected with us on social networks:</span>
-      </div>
-      <!-- Left -->
+                <!-- Right -->
+                <div>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-google"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a href="" class="text-white me-4">
+                        <i class="fab fa-github"></i>
+                    </a>
+                </div>
+                <!-- Right -->
+            </section>
+            <!-- Section: Social media -->
 
-      <!-- Right -->
-      <div>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-google"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-instagram"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-linkedin"></i>
-        </a>
-        <a href="" class="text-white me-4">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
-      <!-- Right -->
-    </section>
-    <!-- Section: Social media -->
+            <!-- Section: Links  -->
+            <section class="">
+                <div class="container text-center text-md-start mt-5">
+                    <!-- Grid row -->
+                    <div class="row mt-3">
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                            <!-- Content -->
+                            <h6 class="text-uppercase fw-bold">Business
+                            </h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-white">Want to be a seller</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Wakazi Shop</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Terms and Conditions</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">FAQ Center
+                                </a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
 
-    <!-- Section: Links  -->
-    <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold">Business
-            </h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-                <p>
-              <a href="#!" class="text-white">Want to be a seller</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Wakazi Shop</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Terms and Conditions</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">FAQ Center
-              </a>
-            </p>
-          </div>
-          <!-- Grid column -->
+                        <!-- Grid column -->
+                        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Products</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-white">Seller Center</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Privacy Policy</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Terms and Conditions</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">FAQ Center
+                                </a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Products</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-white">Seller Center</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Privacy Policy</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Terms and Conditions</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">FAQ Center
-              </a>
-            </p>
-          </div>
-          <!-- Grid column -->
+                        <!-- Grid column -->
+                        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Useful links</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p>
+                                <a href="#!" class="text-white">Your Account</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Become an Affiliate</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Shipping Rates</a>
+                            </p>
+                            <p>
+                                <a href="#!" class="text-white">Help</a>
+                            </p>
+                        </div>
+                        <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Useful links</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-white">Your Account</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Become an Affiliate</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Shipping Rates</a>
-            </p>
-            <p>
-              <a href="#!" class="text-white">Help</a>
-            </p>
-          </div>
-          <!-- Grid column -->
+                        <!-- Grid column -->
+                        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                            <!-- Links -->
+                            <h6 class="text-uppercase fw-bold">Contact</h6>
+                            <hr class="mb-4 mt-0 d-inline-block mx-auto" style="width: 60px; background-color: #7c4dff; height: 2px" />
+                            <p><i class="fas fa-home mr-3"></i> Nairobi, Kenya</p>
+                            <p><i class="fas fa-envelope mr-3"></i> hexanetsystems@gmail.com</p>
+                            <p><i class="fas fa-print mr-3"></i> +254 705 02 7335</p>
+                            <p><i class="fas fa-print mr-3"></i> +254 705 02 7335</p>
+                        </div>
+                        <!-- Grid column -->
+                    </div>
+                    <!-- Grid row -->
+                </div>
+            </section>
+            <!-- Section: Links  -->
 
-          <!-- Grid column -->
-          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Contact</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p><i class="fas fa-home mr-3"></i> Nairobi, Kenya</p>
-            <p><i class="fas fa-envelope mr-3"></i> hexanetsystems@gmail.com</p>
-            <p><i class="fas fa-print mr-3"></i> +254 705 02 7335</p>
-            <p><i class="fas fa-print mr-3"></i> +254 705 02 7335</p>
-          </div>
-          <!-- Grid column -->
-        </div>
-        <!-- Grid row -->
-      </div>
-    </section>
-    <!-- Section: Links  -->
+            <!-- Copyright -->
+            <div class="text-center p-3" style="background-color: #C435E8">
+                © 2024 Copyright:
+                <a class="text-white" href="https://wakazi.co.ke/">Wakazi Works</a>
+            </div>
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
 
-    <!-- Copyright -->
-    <div
-         class="text-center p-3"
-         style="background-color: #C435E8"
-         >
-      © 2024 Copyright:
-      <a class="text-white" href="https://wakazi.co.ke/"
-         >Wakazi Works</a
-        >
     </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
-
-</div>
+</section>
 <!-- End of .container -->
 <!-- End of .container -->
 <!-- Bootstrap core JS-->
