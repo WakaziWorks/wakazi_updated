0ad4cedc318a8a39425dc3c0d08d4558bffe57ce
skeeperloyaltie <gugod254@gmail.com>
2024-04-30 13:39:11 +0300

    debugging erors

--- artisans/__dashboard/index.php
+++ artisans/__dashboard/index.php
@@ -55,6 +55,8 @@
                     </div>
 
                     <?php
+                    error_reporting(E_ALL);
+                    ini_set('display_errors', 1);
                     session_start();
 
                     if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
