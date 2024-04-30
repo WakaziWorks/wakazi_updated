db0cef569b355ee97539784bb9681343fd8bdce1
skeeperloyaltie <gugod254@gmail.com>
2024-04-30 12:17:46 +0300

    updated the footer to match the inheritance

---
+++ screens/footer/footer.php
@@ -0,0 +1,127 @@
+<section class="footer-top" id="footer-1" style="background-color: #e868f0;">
+    <div class="container text-center">
+        <div class="row align-items-center flex-column flex-sm-row">
+            <div class="col footer-col">
+                <img src="static/images/x-diamond-fill.svg" />
+                <h5 style="color: #000; font-weight: normal;">Great Value</h5>
+                <p class="col-p text-light">We offer competitive prices on our product range.</p>
+            </div>
+            <div class="col footer-col">
+                <img src="static/images/credit-card-2-front-fill.svg" />
+                <h5 style="color: #000; font-weight: normal;">Safe Payment</h5>
+                <p class="col-p text-light">Pay with the world’s most popular and secure payment methods.</p>
+            </div>
+            <div class="col footer-col">
+                <img src="static/images/shield-lock-fill.svg" />
+                <h5 style="color: #000; font-weight: normal;">Shop with Confidence</h5>
+                <p class="col-p text-light">Our Buyer Protection covers your purchase from click to delivery</p>
+            </div>
+            <div class="col footer-col">
+                <img src="static/images/question-circle-fill.svg" />
+                <h5 style="color: #000; font-weight: normal;">24/7 Help Center</h5>
+                <p class="col-p text-light">Round-the-clock assistance for a smooth shopping experience.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Footer-->
+    <section class="footer" style="background-color: #c837d1;">
+        <div class="container-fluid">
+            <div class="row" style="margin-left: 7em;">
+                <div class="col terms-section">
+                    <h4>Shopping Guide</h4>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">How do I pay on wakazi?</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">How long does my order arrive?</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">How to shop on wakazi?</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Forgot password?</p>
+                    </a>
+                </div>
+                <div class="col terms-section">
+                    <h4>Customer Help</h4>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Privacy Policy</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Terms and Conditions</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Account Settings</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Delivery and Shipping</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">FAQ Center</p>
+                    </a>
+                </div>
+                <div class="col terms-section">
+                    <h4>Business</h4>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Want to be a Seller?</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Seller Center</p>
+                    </a>
+                    <a href="_superadmin_/index.html">
+                        <p class="text-light">Wakazi Shop</p>
+                        </a>
+                </div>
+                <div class="col terms-section">
+                    <h4>Stay Connected</h4>
+                    <a href="https://www.youtube.com/@WakaziWorks" target="blank"><i class="bi bi-youtube text-light"></i></a>
+                    <a href="https://ke.linkedin.com/company/wakazi-works-platform" target="blank"><i class="bi bi-linkedin text-light"></i></a>
+                    <a href="https://twitter.com/wakaziworks" target="blank"><i class="bi bi-twitter text-light"></i></i></a>
+                    <a href="https://www.instagram.com/wakazi_works/" target="blank"><i class="bi bi-instagram text-light"></i></a>
+                </div>
+    </section>
+    <div class="bottom-footer" style="background: #7d1a86">
+        <div>
+            <div class="mb-2 text-light">&copy; Wakazi Works 2024</div>
+            <a href="_superadmin_/index.html">Privacy</a>
+            <span class="mx-2 text-light">&middot;</span>
+            <a href="_superadmin_/index.html">Terms</a>
+            <span class="mx-2 text-light">&middot;</span>
+            <a href="_superadmin_/index.html">FAQ</a>
+        </div>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+    </html>
+
+            <!-- Copyright -->
+        </footer>
+        <!-- Footer -->
+
+    </div>
+</section>
+<!-- End of .container -->
+<!-- End of .container -->
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<!-- * *                               SB Forms JS                               * *-->
+<!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+<!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
