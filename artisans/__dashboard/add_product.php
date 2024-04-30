b321469bfb77cbfbefa57cb8b4d33666aa0f0268
skeeperloyaltie <gugod254@gmail.com>
2024-04-30 11:47:16 +0300

    Confirmation of logins from config.php

--- artisans/__dashboard/add_product.php
+++ artisans/__dashboard/add_product.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require '../_auth/__config/config.php'; // Include database configuration
+require '../__auth/__config/config.php'; // Include database configuration
 
 session_start(); // Start the session
 
