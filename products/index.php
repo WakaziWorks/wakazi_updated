7f5c7d68cd0b7182be4d7572505af9549839f041
skeeperloyaltie <gugod254@gmail.com>
2024-05-04 19:03:56 +0300

    fix  approve error

--- products/index.php
+++ products/index.php
@@ -32,24 +32,22 @@ include("../screens/headers/header.php");
                     // Loop through each product and display its details
                     while ($row = $result->fetch_assoc()) {
                         echo "<div class='col product-details'>";
-                        // Check if the image data exists
                         if (!empty($row['image'])) {
-                            // Convert the binary image data to base64 format
                             $imageData = base64_encode($row['image']);
-                            // Output the image using base64 format
                             echo "<img src='data:image/jpeg;base64,{$imageData}' alt='Product' />";
                         } else {
-                            // If no image data is available, display a placeholder image
                             echo "<img src='placeholder.jpg' alt='Product Placeholder' />";
                         }
                         echo "<p class='description'>{$row['description']}</p>";
                         echo "<div class='product-price'>KES. {$row['Price']}</div>";
-                        echo "<div class='cart-button'>";
-                        echo "<button><i class='bi bi-plus'></i>Add to Cart</button>";
-                        echo "<a href=''>See more <i class='bi bi-arrow-right' style='font-weight: bold; font-size: large;'></i></a>";
-                        echo "</div>";
+                        echo "<form action='add_to_cart.php' method='post'>";
+                        echo "<input type='hidden' name='product_id' value='{$row['ProductID']}'>";
+                        echo "<button type='submit' class='btn btn-primary'><i class='bi bi-plus'></i> Add to Cart</button>";
+                        echo "</form>";
+                        echo "<a href='product_details.php?id={$row['ProductID']}'>See more <i class='bi bi-arrow-right'></i></a>";
                         echo "</div>";
                     }
+                    
 
                     // Close the database connection
                     $mysqli->close();
