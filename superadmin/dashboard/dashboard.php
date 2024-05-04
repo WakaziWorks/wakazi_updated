fb54d5aa8c5516a57b388a8d3524c66d119b7cb3
skeeperloyaltie <gugod254@gmail.com>
2024-05-04 20:56:25 +0300

    fix  approve error

--- superadmin/dashboard/dashboard.php
+++ superadmin/dashboard/dashboard.php
@@ -292,13 +292,13 @@ if (!isset($_SESSION["logged_in"]) || $_SESSION["logged_in"] !== true) {
                 <div class="bg-light rounded-top p-4">
                     <div class="row">
                         <div class="col-12 col-sm-6 text-center text-sm-start">
-                            &copy; <a href="#">Your Site Name</a>, All Right Reserved.
+                            &copy; <a href="#">Wakazi Works</a>, All Right Reserved.
                         </div>
                         <div class="col-12 col-sm-6 text-center text-sm-end">
                             <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                            Designed By <a href="https://htmlcodex.com">HTML Codex</a>
+                            Designed By <a href="">Wakazi Systems</a>
                             </br>
-                            Distributed By <a class="border-bottom" href="https://themewagon.com" target="_blank">ThemeWagon</a>
+                            Distributed By <a class="border-bottom" href="" target="_blank">Wakazi Works</a>
                         </div>
                     </div>
                 </div>
