b067f8126730bcd51c218f87018f8a7b54b0175c
AbiollaghJames <abiollajames@protonmail.com>
2024-05-06 01:34:30 +0300

    Cart pages implemented awaiting Gudah's magic

--- cart/cart.php
+++ cart/cart.php
@@ -13,14 +13,14 @@ include("../screens/headers/header.php");
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid  bg-secondary bg-opacity-50 w-75 rounded p-3 d-flex mb-6 flex-column justify-content-center align-items-center" style="margin-top: 10em;">
-        <div class="text-center bg-light rounded-pill p-2 d-flex mt-5 justify-content-center align-items-center" style="height: 9em; width: 9em;">
-            <i class="bi bi-cart3 fs-1 fw-bold text-black"></i>
+    <div class="container-fluid  bg-light border w-75 rounded p-3 d-flex mb-6 flex-column justify-content-center align-items-center" style="margin-top: 10em;">
+        <div class="text-center rounded-pill p-2 d-flex mt-5 justify-content-center align-items-center" style="background: #c837d1; height: 9em; width: 9em;">
+            <i class="bi bi-cart3 fs-1 fw-bold text-white"></i>
         </div>
         <div class="text-center text-black mt-3">
             <h3>Your cart is empty!</h3>
             <p>Browse our products to discover our best deals!</p>
-            <a href="../../products/index.php"><button type="button" class="btn btn-secondary btn-lg mt-5 mb-5">START SHOPPING</button></a>
+            <a href="../../products/index.php"><button type="button" class="btn btn-lg mt-5 mb-5" style="background: #c837d1; color: #fff;">START SHOPPING</button></a>
         </div>
     </div>
 </body>
