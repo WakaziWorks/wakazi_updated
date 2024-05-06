7772b5b4c08f344b656e649740b2056a09e5613e
skeeperloyaltie <gugod254@gmail.com>
2024-05-07 00:43:52 +0300

    fix header for phone visibility

--- screens/headers/cust.php
+++ screens/headers/cust.php
@@ -3,19 +3,19 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
 
-$response = ['logged_in' => false];
-if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
-    $response['logged_in'] = true;
-}
+// $response = ['logged_in' => false];
+// if (isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true) {
+//     $response['logged_in'] = true;
+// }
 
 
-$isLoggedIn = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
-if (!isset($_SESSION['cart'])) {
-    $_SESSION['cart'] = [];
-}
-if (isset($_SESSION['flash'])) {
-    echo '<div class="alert alert-success" role="alert">' . $_SESSION['flash'] . '</div>';
-    unset($_SESSION['flash']);
-}
+// $isLoggedIn = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true;
+// if (!isset($_SESSION['cart'])) {
+//     $_SESSION['cart'] = [];
+// }
+// if (isset($_SESSION['flash'])) {
+//     echo '<div class="alert alert-success" role="alert">' . $_SESSION['flash'] . '</div>';
+//     unset($_SESSION['flash']);
+// }
 // echo json_encode($response);
 ?>
