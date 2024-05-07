2aa665f4dd460055316ac621b7284ebab3011743
skeeperloyaltie <gugod254@gmail.com>
2024-05-08 00:38:25 +0300

    fix header for phone visibility

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include("screens/headers/header.php")
 ?>
-<div class="container-fluid py-5">
+<div class="container-fluid py-5" style="height:300px;">
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
