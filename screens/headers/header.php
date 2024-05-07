68fe5bc65aa8f53106106c54993fa7b2fcd0ba12
skeeperloyaltie <gugod254@gmail.com>
2024-05-07 18:16:13 +0300

    fix header for phone visibility

--- screens/headers/header.php
+++ screens/headers/header.php
@@ -206,20 +206,7 @@ require "cust.php";
 
     <!-- ======= Header ======= -->
    <!-- ======= Header ======= -->
-   <section id="topbar" class="topbar d-flex align-items-center">
-    <div class="container d-flex justify-content-center justify-content-md-between">
-      <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">skeepertech@gmail.com</a></i>
-        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+254 702940509</span></i>
-      </div>
-      <div class="social-links d-none d-md-flex align-items-center">
-        <a href="https://www.twitter.com/skeeperloyaltie" class="twitter"><i class="bi bi-twitter"></i></a>
-        <a href="https://wa.me/+254702940509" class="facebook"><i class="bi bi-whatsapp"></i></a>
-        <!-- <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> -->
-        <!-- <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a> -->
-      </div>
-    </div>
-  </section><!-- End Top Bar -->
+   
     <!-- End Top Bar -->
     <header class="header">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
