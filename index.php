99382e5942a3526380c7e8bac69ecf500d5c4f2a
skeeperloyaltie <gugod254@gmail.com>
2024-05-08 01:47:01 +0300

    fix header for phone visibility

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ include("screens/headers/header.php")
             <div class="carousel-item text-center p-4" data-bs-interval="3000">
                 <video src="static/images/Handtools.mp4" class="" autoplay muted loop></video>
                 <div class="carousel-caption justify-content-center">
-                <h1 class="text-start">The silent Philosophers.</h1>
+                    <h1 class="text-start">The silent Philosophers.</h1>
                     <p class="text-start">Artisans are the silent philosophers of society, shaping the world with their hands and minds.</p>
                     <button class="btn text-uppercase" style="background: #c837d1; font-weight: bold; border-radius: 100px; padding: 10px;"><a href="#collection" class="text-white" style="text-decoration: none;">Discover More</a></button>
                 </div>
@@ -173,8 +173,8 @@ include("screens/headers/header.php")
 
 <section id="features">
     <div class="container px-5">
-           <!-- Gradient title -->
-           <div class="row">
+        <!-- Gradient title -->
+        <div class="row">
             <div class="col-12">
                 <h2 class="text-center font-weight-bold my-5" style="background: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
                     -webkit-background-clip: text;
@@ -189,15 +189,15 @@ include("screens/headers/header.php")
                     <div class="row gx-5">
                         <div class="col-md-6 mb-5">
                             <div class="text-center">
-                            <img src="static/img/touch-screen_10605241.png" width="90px" height="90px">
-                            <br>
-                            <h3 class="font-alt">Product Sales</h3>
+                                <img src="static/img/touch-screen_10605241.png" width="90px" height="90px">
+                                <br>
+                                <h3 class="font-alt">Product Sales</h3>
                                 <p class="text-muted mb-0">Extreme product functionalities</p>
                             </div>
                         </div>
                         <div class="col-md-6 mb-5">
                             <div class="text-center">
-                            <img src="static/img/delivery_6137526 (1).png" width="90px" height="90px">
+                                <img src="static/img/delivery_6137526 (1).png" width="90px" height="90px">
                                 <h3 class="font-alt">Vendor Flexibility</h3>
                                 <p class="text-muted mb-0">All vendors with flexible product sales!</p>
                             </div>
@@ -206,17 +206,17 @@ include("screens/headers/header.php")
                     <div class="row">
                         <div class="col-md-6 mb-5 mb-md-0">
                             <div class="text-center">
-                            <img src="static/img/touch-screen_10605241.png" width="90px" height="90px">
-                            <br>
-                            <h3 class="font-alt">Free to Use</h3>
+                                <img src="static/img/touch-screen_10605241.png" width="90px" height="90px">
+                                <br>
+                                <h3 class="font-alt">Free to Use</h3>
                                 <p class="text-muted mb-0">Flexible system</p>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="text-center">
-                            <img src="static/img/shield_508281.png" width="90px" height="90px">
-                            <br>
-                            <h3 class="font-alt">Security</h3>
+                                <img src="static/img/shield_508281.png" width="90px" height="90px">
+                                <br>
+                                <h3 class="font-alt">Security</h3>
                                 <p class="text-muted mb-0">Secure product checkout!</p>
                             </div>
                         </div>
@@ -241,8 +241,10 @@ include("screens/headers/header.php")
             <style>
                 .device .screen {
                     position: relative;
-                    width: 100%; /* Adjust the size as needed */
-                    padding-top: 100%; /* Equal to width for a perfect circle */
+                    width: 100%;
+                    /* Adjust the size as needed */
+                    padding-top: 100%;
+                    /* Equal to width for a perfect circle */
                     border-radius: 50%;
                     overflow: hidden;
                 }
@@ -264,22 +266,24 @@ include("screens/headers/header.php")
 
 
 <section class="bg-light">
-    <div class="container px-5">
-        <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
-            <div class="col-12 col-lg-5">
-                <h2 class="display-4 lh-1 mb-4">Renaissance fuses Avant-Garde</h2>
-                <p class="lead fw-normal text-muted mb-4">In the vibrant tapestry of artistic expression, where the Renaissance meets the avant-garde, lies a fusion of tradition and innovation that ignites the senses.</p>
-                <div class="collapse mb-4" id="collapseExample">
-                    <p class="lead fw-normal text-muted">
-                        Picture the sun-drenched landscapes of sub-Saharan Africa, where rhythms pulse through the earth and colors dance upon the canvas of existence. Here, amidst the ancient rhythms of tribal drums and the intricate beadwork of cultural heritage, emerges a new Renaissance—a rebirth of creativity that transcends boundaries.
-                        
-                        In this eclectic convergence, traditional motifs intertwine with modern techniques, birthing a kaleidoscope of expression that challenges the norms of convention. This is where the echoes of history harmonize with the avant-garde spirit, where every brushstroke whispers tales of resilience and every melody carries the heartbeat of a continent.
-                    </p>
+    <div class="container-fluid">
+        <div class="container px-5">
+            <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+                <div class="col-12 col-lg-5">
+                    <h2 class="display-4 lh-1 mb-4">Renaissance fuses Avant-Garde</h2>
+                    <p class="lead fw-normal text-muted mb-4">In the vibrant tapestry of artistic expression, where the Renaissance meets the avant-garde, lies a fusion of tradition and innovation that ignites the senses.</p>
+                    <div class="collapse mb-4" id="collapseExample">
+                        <p class="lead fw-normal text-muted">
+                            Picture the sun-drenched landscapes of sub-Saharan Africa, where rhythms pulse through the earth and colors dance upon the canvas of existence. Here, amidst the ancient rhythms of tribal drums and the intricate beadwork of cultural heritage, emerges a new Renaissance—a rebirth of creativity that transcends boundaries.
+
+                            In this eclectic convergence, traditional motifs intertwine with modern techniques, birthing a kaleidoscope of expression that challenges the norms of convention. This is where the echoes of history harmonize with the avant-garde spirit, where every brushstroke whispers tales of resilience and every melody carries the heartbeat of a continent.
+                        </p>
+                    </div>
+                    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Read More</a>
                 </div>
-                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">Read More</a>
-            </div>
-            <div class="col-sm-8 col-md-6">
-                <div class="px-5 px-sm-0"><img class="img-fluid rounded-circle" src="static/images/igor.jpg" alt="Artistic Image" /></div>
+                <div class="col-sm-8 col-md-6">
+                    <div class="px-5 px-sm-0"><img class="img-fluid rounded-circle" src="static/images/igor.jpg" alt="Artistic Image" /></div>
+                </div>
             </div>
         </div>
     </div>
