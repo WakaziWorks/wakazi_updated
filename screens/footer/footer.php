5c4512d38e066b2cdd8dea1d273e4ba351523517
skeeperloyaltie <gugod254@gmail.com>
2024-05-08 02:00:14 +0300

    fix header for phone visibility

--- screens/footer/footer.php
+++ screens/footer/footer.php
@@ -1,6 +1,11 @@
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <style>
         .footer-col {
             transition: background-color 0.7s ease;
@@ -26,6 +31,12 @@
             cursor: pointer;
         }
     </style>
+  </head>
+
+
+
+
+    
 
     <section class="footer-top" id="footer-1" style="background-color: #e868f0; padding: 50px;">
         <div class="container text-center">
