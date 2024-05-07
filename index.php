c08a72f5daf8f02bdd969cbcd337c3a3a73c8b4f
skeeperloyaltie <gugod254@gmail.com>
2024-05-07 18:49:14 +0300

    fix header for phone visibility

--- index.php
+++ index.php
@@ -3,135 +3,137 @@ include("screens/headers/header.php")
 ?>
 <div class="container-fluid" style="padding: 20px;">
 
-        <div id="carouselExampleCaptions" class="carousel slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner ratio ratio-21x9">
-                <div class="carousel-item active" data-bs-interval="3000">
-                    <video src="static/images/Universe.mp4" class="" autoplay muted loop></video>
-                    <div class="carousel-caption justify-content-center">
-                        <h1 class="heading-h1">Artisans are the Alchemists.</h1>
-                        <p>Artisans are the alchemists of our time, turning ordinary materials into works of wonder.</p>
-                        <button style="font-size: 1.5em; background: #c837d1; font-weight: bold; border-radius: 100px; padding: 20px;"><a href="#collection">DISCOVER MORE</a></button>
-                    </div>
-                </div>
-                <div class="carousel-item" data-bs-interval="3000">
-                    <video src="static/images/Handtools.mp4" class="" autoplay muted loop></video>
-                    <div class="carousel-caption justify-content-center">
-                        <h1 class="heading-h1">The silent Philosophers.</h1>
-                        <p>Artisans are the silent philosophers of society, shaping the world with their hands and minds. </p>
-                        <button style=" background: #c837d1; font-weight: bold; border-radius: 100px; padding: 10px;"><a href="#collection">DISCOVER MORE</a></button>
-                    </div>
-                </div>
-                <div class="carousel-item" data-bs-interval="3000">
-                    <video src="static/images/Pottery.mp4" class="" autoplay muted loop></video>
-                    <div class="carousel-caption justify-content-center">
-                        <h1 class="heading-h1">The true Artisan.</h1>
-                        <p>The true artisan is a seeker of beauty and truth, a philosopher of the tangible.</p>
-                        <button style="font-size: 1.5em; background: #c837d1; font-weight: bold; border-radius: 100px; padding: 20px;"><a href="#collection">DISCOVER MORE</a></button>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+    <div id="carouselExampleCaptions" class="carousel slide">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
+        <div class="carousel-inner ratio ratio-21x9">
+            <div class="carousel-item active" data-bs-interval="3000">
+                <video src="static/images/Universe.mp4" class="" autoplay muted loop></video>
+                <div class="carousel-caption justify-content-center">
+                    <h1 class="heading-h1">Artisans are the Alchemists.</h1>
+                    <p>Artisans are the alchemists of our time, turning ordinary materials into works of wonder.</p>
+                    <button style="font-size: 1.5em; background: #c837d1; font-weight: bold; border-radius: 100px; padding: 20px;"><a href="#collection">DISCOVER MORE</a></button>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="3000">
+                <video src="static/images/Handtools.mp4" class="" autoplay muted loop></video>
+                <div class="carousel-caption justify-content-center">
+                    <h1 class="heading-h1">The silent Philosophers.</h1>
+                    <p>Artisans are the silent philosophers of society, shaping the world with their hands and minds. </p>
+                    <button style=" background: #c837d1; font-weight: bold; border-radius: 100px; padding: 10px;"><a href="#collection">DISCOVER MORE</a></button>
+                </div>
+            </div>
+            <div class="carousel-item" data-bs-interval="3000">
+                <video src="static/images/Pottery.mp4" class="" autoplay muted loop></video>
+                <div class="carousel-caption justify-content-center">
+                    <h1 class="heading-h1">The true Artisan.</h1>
+                    <p>The true artisan is a seeker of beauty and truth, a philosopher of the tangible.</p>
+                    <button style="font-size: 1.5em; background: #c837d1; font-weight: bold; border-radius: 100px; padding: 20px;"><a href="#collection">DISCOVER MORE</a></button>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
 </div>
+</div>
 
+<div class="container-fluid" style="padding: 20px;">
 
-<div class="container-fluid" style="background: linear-gradient(to bottom, #6c2e8e, #ffffff); padding: 25px;">
-    <div class="row justify-content-center">
-        <div class="col-md-9">
-            <h1 class="text-center mb-5">Shop by collections</h1>
-            <div class="row gx-3">
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('jewellery')">
-                            <img src="static/images/jewellery.webp" class="card-img-top" alt="Jewellery">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Jewellery</h4>
+    <div class="container-fluid" style="background: linear-gradient(to bottom, #6c2e8e, #ffffff); padding: 25px;">
+        <div class="row justify-content-center">
+            <div class="col-md-9">
+                <h1 class="text-center mb-5">Shop by collections</h1>
+                <div class="row gx-3">
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('jewellery')">
+                                <img src="static/images/jewellery.webp" class="card-img-top" alt="Jewellery">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Jewellery</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('home_decor')">
+                                <img src="static/images/decor.jpg" class="card-img-top" alt="Home and Decor">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Home Decors</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('bags_purses')">
+                                <img src="static/images/bag.webp" class="card-img-top" alt="Bags and Purses">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Bags and Purses</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('art_collectibles')">
+                                <img src="static/images/art2.jpg" class="card-img-top" alt="Art and Collectibles">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Art and Collectibles</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('home_decor')">
-                            <img src="static/images/decor.jpg" class="card-img-top" alt="Home and Decor">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Home Decors</h4>
+                <div class="row mt-4 gx-3">
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('books_movies_music')">
+                                <img src="static/images/books.jpg" class="card-img-top" alt="Books, Movies and Music">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Books, Movies and Music</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('bags_purses')">
-                            <img src="static/images/bag.webp" class="card-img-top" alt="Bags and Purses">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Bags and Purses</h4>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('electronics')">
+                                <img src="static/images/electronics.jpg" class="card-img-top" alt="Electronics">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Electronics</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('art_collectibles')">
-                            <img src="static/images/art2.jpg" class="card-img-top" alt="Art and Collectibles">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Art and Collectibles</h4>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('accessories')">
+                                <img src="static/images/accesories.jpg" class="card-img-top" alt="Accessories">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Accessories</h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row mt-4 gx-3">
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('books_movies_music')">
-                            <img src="static/images/books.jpg" class="card-img-top" alt="Books, Movies and Music">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Books, Movies and Music</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('electronics')">
-                            <img src="static/images/electronics.jpg" class="card-img-top" alt="Electronics">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Electronics</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('accessories')">
-                            <img src="static/images/accesories.jpg" class="card-img-top" alt="Accessories">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Accessories</h4>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 col-lg-3">
-                    <div class="card text-center">
-                        <a href="#" onclick="redirectToCategory('craft_supplies_tools')">
-                            <img src="static/images/craft.jpg" class="card-img-top" alt="Craft supplies and Tools">
-                        </a>
-                        <div class="card-body">
-                            <h4 class="card-title">Craft supplies and Tools</h4>
+                    <div class="col-12 col-md-6 col-lg-3">
+                        <div class="card text-center">
+                            <a href="#" onclick="redirectToCategory('craft_supplies_tools')">
+                                <img src="static/images/craft.jpg" class="card-img-top" alt="Craft supplies and Tools">
+                            </a>
+                            <div class="card-body">
+                                <h4 class="card-title">Craft supplies and Tools</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
