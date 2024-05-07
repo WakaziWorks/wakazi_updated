acd390e7ade37da2194a6a2782d2a988e2abcc23
skeeperloyaltie <gugod254@gmail.com>
2024-05-07 18:08:38 +0300

    fix header for phone visibility

--- screens/headers/header.php
+++ screens/headers/header.php
@@ -17,6 +17,21 @@ require "cust.php";
     <!-- Bootstrap CSS -->
     <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> -->
 
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Raleway:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Bootstrap Bundle with Popper -->
@@ -189,8 +204,8 @@ require "cust.php";
 <body>
     <div id="alert-placeholder"></div>
 
-  <!-- ======= Header ======= -->
-  <!-- <section id="topbar" class="topbar d-flex align-items-center">
+    <!-- ======= Header ======= -->
+    <!-- <section id="topbar" class="topbar d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
         <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:hexanetsystems@gmail.com">Gmail</a></i>
@@ -200,11 +215,11 @@ require "cust.php";
         <a href="https://www.twitter.com/wakaziworks" class="twitter"><i class="bi bi-twitter"></i></a>
         <a href="" class="facebook"><i class="bi bi-whatsapp"></i></a>
         <a href="#" class="instagram"><i class="bi bi-instagram"></i></a> -->
-        <!-- <<a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+    <!-- <<a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
       </div>
     </div>
   </section>  -->
-  <!-- End Top Bar -->
+    <!-- End Top Bar -->
     <header class="header">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
@@ -307,4 +322,4 @@ require "cust.php";
         </nav>
     </header>
     <div class="main">
-            <div class="container-fluid">
+        <div class="container-fluid">
