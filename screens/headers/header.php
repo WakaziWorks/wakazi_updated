10678e1f65940b763cf9378cd62e3d37a4c1d22f
skeeperloyaltie <gugod254@gmail.com>
2024-05-08 12:39:29 +0300

    padding shop by collection

--- screens/headers/header.php
+++ screens/headers/header.php
@@ -330,7 +330,7 @@ require "cust.php";
 
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <i class="bi bi-person"></i> <?php $_SESSION['name']; ?>
+                                    <?php $_SESSION['name']; ?>
                                 </a>
                                 <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                                     <li><a class="dropdown-item" href="#">My Profile</a></li>
